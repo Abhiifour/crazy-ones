48f6f6ce80b926d667189985de75a87ba8ede982
abhiifour <as8998013@gmail.com>
2025-05-02 16:18:23 +0530

    fixed routing and more ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+"use client"
+import { useRouter } from "next/navigation";
 // import Blog from "./blogs/page";
 
 export default function Home() {
+  const router = useRouter()
   return (
-    <div>
-      {/* <Blog/> */}
+    <div className="flex flex-col items-center">
+      <div className="flex justify-center items-center gap-2 mt-20">
+      <h1 className="uppercase text-xl cursor-pointer" onClick={() => router.push('/quote')}>General</h1>
+      <h1>/</h1>
+      <h1 className="uppercase text-xl text-primary/40">philosophical</h1>
+      </div>
     </div>
   );
 }
